f6c94cf9187391790760d43b80c347821c89e85d
ss-raicangu <49044752+ss-raicangu@users.noreply.github.com>
2019-05-01 20:24:17 +1200

    Removed unnecessary comments from clock_script.js and adjusted style of document. Also converted logo to jpg because it is smaller image.

--- clock_script.js
+++ clock_script.js
@@ -1,20 +1,12 @@
 const clocks = document.getElementsByClassName('clock');
 
-//updateTime is a function that uses moment.js library to handle time and dates easily
+// updateTime is a function that uses moment.js library to handle time and dates easily
 function updateTime() {
-    console.log('Im working!');
     for (let clock of clocks) {
-
-        // var now;
-        // if (typeof clock.id == "string") {
-        const now = moment(new Date()).tz(clock.id);
-        // } else {
-        //     now = moment(); //generates the current date and time
-        // }
-        const humanReadable = now.format('HH:mm:ss'); //display in hours:minutes:seconds
-        clock.textContent = humanReadable; //the text content of the HTML is set to whatever humanReadable holds
+        clock.textContent = clock.id + "\n" + moment(new Date()).tz(clock.id).format("HH:mm");
     }
 }
 
-setInterval(updateTime, 1000); //setInterval is a function that calls a function every x seconds
-updateTime();
+// setInterval is a function that calls a function every x seconds
+setInterval(updateTime, 60000);
+updateTime(); // need to update immediately at first.
